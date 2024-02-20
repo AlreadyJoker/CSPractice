32f95d9595ac9e13d598234f78852ae56ebdb776
张学崇 <1125754903@qq.com>
2024-02-20 20:43:23 +0800

    练习

--- CSharp/Practice/Program.cs
+++ CSharp/Practice/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, GitHub");
+            CSharpLevel1.Practice();
         }
     }
 }
